39c176927472409235cdc8fbb6ed68cd4bdd745a
sultan-07 <sultanalih8@gmail.com>
2025-03-19 00:05:26 +0530

    deleted few lines

--- src/components/TrendingCard.jsx
+++ src/components/TrendingCard.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import {Link,NavLink} from 'react-router'
+import React, {  useState } from 'react'
+import {Link} from 'react-router'
 import { useProduct } from '../context'
 import {useLocation} from 'react-router'
 
