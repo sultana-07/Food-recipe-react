4927bfc43ae267aaf420679bebdb890f6588ba76
sultan-07 <sultanalih8@gmail.com>
2025-04-04 00:15:23 +0530

    minor change

--- src/components/TrendingCard.jsx
+++ src/components/TrendingCard.jsx
@@ -30,8 +30,8 @@ function TrendingCard({names,instructions,image = null,btn,id = "",item }) {
 
   return (
    <>
-   <div className="w-full md:max-w-60 md:h-80 flex flex-wrap h-40 rounded bg-white overflow-hidden shadow-2xl hover:scale-105 duration-200">
-  <div className='w-full flex md:flex-col'>
+   <div className="w-full md:max-w-60 md:h-80 flex flex-wrap h-fit rounded  overflow-hidden shadow-2xl hover:scale-105 duration-200 ">
+  <div className='w-full flex md:flex-col p-2'>
   <img className="w-40 md:w-full h-40" src={image ? image : "https://icrier.org/wp-content/uploads/2022/12/media-Event-Image-Not-Found.jpg" } alt="Sunset in the mountains"/>
   <div className="px-6 py-4 w-full ">
     <div className="font-bold text-xl mb-2">{names.slice(0,14)}</div>
